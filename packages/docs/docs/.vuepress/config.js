1ebf890d5abfc41495c1d37d13c58d9153a5c504
卡卡查克 <282281884@qq.com>
2022-07-01 22:30:35 +0800

    docs: 新增vue press

---
+++ packages/docs/docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  title: "ZVM",
+  description: "mini-vue",
+  dest: "./dist",
+  port: "8080",
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    sidebar: "auto",
+    nav: [
+      {
+        text: "指南",
+        link: "/guide/",
+      },
+      {
+        text: "展示",
+        link: "http://101.43.155.53/front_end_baidu/mini_mvvm/",
+      },
+      {
+        text: "github",
+        link: "https://github.com/kakachake",
+      },
+    ],
+    sidebar: {
+      "/guide/": [["/guide/", "指南"]],
+    },
+    sidebarDepth: 2,
+    lastUpdated: "Last Updated",
+    searchMaxSuggestoins: 10,
+    serviceWorker: {
+      updatePopup: {
+        message: "有新的内容.",
+        buttonText: "更新",
+      },
+    },
+    editLinks: true,
+    editLinkText: "在 GitHub 上编辑此页 ！",
+  },
+};
---
+++ packages/docs/docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+
+heroText: Welcome to ZVM.
+tagline: mini vue.
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+  # - title: test
+  #   details: test
+
+footer: Copyright © 2021-present Kakachake
+---
